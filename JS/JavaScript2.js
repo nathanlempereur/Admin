f1f8b27fa4d9b7d77972520d90ff8d30913d3895
nathan lempereur <167067383+nathanlempereur@users.noreply.github.com>
2024-04-15 18:26:49 +0200

    Update JavaScript2.js
    correction de beug de connexion

--- JS/JavaScript2.js
+++ JS/JavaScript2.js
@@ -1,23 +1,24 @@
-		function validateForm() {
-			// Récupération des valeurs des champs d'identifiant et de mot de passe
-			var username = document.getElementById("username").value;
-			var password = document.getElementById("password").value;
-		
-			// Définition des identifiants et mots de passe autorisés
-			var credentials = {
-				"admin": "admin",
-				"lucaster62": "123",
-				// Ajoutez d'autres identifiants et mots de passe au besoin
-			};
+// Fonction pour valider le formulaire de connexion
+function validateForm() {
+    // Récupération des valeurs des champs d'identifiant et de mot de passe
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
 
-			// Vérification des identifiants
-			if (credentials.hasOwnProperty(username) && credentials[username] === password) {
-				// Redirection vers la page Home.html si les identifiants sont valides
-				window.location.href = "Home.html";
-				return false; // Empêche l'envoi du formulaire
-			} else {
-				// Affichage d'un message d'erreur en cas d'identifiants incorrects
-				alert("Identifiant ou mot de passe incorrect.");
-				return false; // Empêche l'envoi du formulaire
-			}
-		}
+    // Définition des identifiants et mots de passe autorisés
+    var credentials = {
+        "admin": "admin",
+        "lucaster62": "123",
+        // Ajoutez d'autres identifiants et mots de passe au besoin
+    };
+
+    // Vérification des identifiants
+    if (credentials.hasOwnProperty(username) && credentials[username] === password) {
+        // Redirection vers la page Home.html avec le paramètre fromLogin
+        window.location.href = "Home.html?fromLogin=true";
+        return false; // Empêche l'envoi du formulaire
+    } else {
+        // Affichage d'un message d'erreur en cas d'identifiants incorrects
+        alert("Identifiant ou mot de passe incorrect.");
+        return false; // Empêche l'envoi du formulaire
+    }
+}
