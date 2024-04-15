5bdca3e2f2845eb8b07e58d94dab924d152eb4e0
nathan lempereur <167067383+nathanlempereur@users.noreply.github.com>
2024-04-15 18:37:16 +0200

    Update JavaScript.js
    abandon

--- JS/JavaScript.js
+++ JS/JavaScript.js
@@ -1,20 +1,3 @@
-// Fonction pour vérifier l'état d'authentification de l'utilisateur
-function checkAuthentication() {
-    // Vérifier si un cookie ou une session indiquant la connexion de l'utilisateur est présent
-    const isAuthenticated = document.cookie.includes("authenticated=true");
-
-    // Si l'utilisateur est authentifié, le rediriger vers la page Home
-    if (isAuthenticated) {
-        window.location.href = "https://nathanlempereur.github.io/Admin/Home.html";
-    } else {
-        // Si l'utilisateur n'est pas authentifié, le rediriger vers la page de connexion
-        window.location.href = "https://nathanlempereur.github.io/Admin/Login.html";
-    }
-}
-
-// Appeler la fonction de vérification de l'authentification lors du chargement de la page
-window.onload = checkAuthentication;
-
 // Fonction pour mettre à jour l'heure
 function updateClock() {
     var now = new Date();
