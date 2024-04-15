fdc11e87f8f1c4bf60d9be9f9dc1a794b0f74035
nathan lempereur <167067383+nathanlempereur@users.noreply.github.com>
2024-04-15 18:12:19 +0200

    Update JavaScript.js
    Ajout de la sécurité via Cookies

--- JS/JavaScript.js
+++ JS/JavaScript.js
@@ -1,3 +1,22 @@
+// Vérifie si l'utilisateur est connecté
+function checkAuthentication() {
+    // Vérifie si un cookie ou une session indiquant la connexion de l'utilisateur est présent
+    // Ici, vous devrez utiliser la méthode appropriée pour vérifier l'état d'authentification de l'utilisateur
+    // Par exemple, si vous utilisez des cookies :
+    const isAuthenticated = document.cookie.includes("authenticated=true");
+
+    // Si l'utilisateur n'est pas authentifié, redirigez-le vers la page de connexion
+    if (!isAuthenticated) {
+        window.location.href = "https://nathanlempereur.github.io/Admin/Login"; // Remplacez "Login.html" par le chemin de votre page de connexion
+    }
+}
+
+// Appeler la fonction de vérification de l'authentification lors du chargement de la page
+window.onload = checkAuthentication;
+
+
+
+
 // Fonction pour mettre à jour l'heure
 function updateClock() {
     var now = new Date();
